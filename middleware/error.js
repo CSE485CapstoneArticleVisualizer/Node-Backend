baea7b0f726e1a8e1fd63269d4d99b9be2d8e1ff
Alex <alexnou@gmail.com>
2019-03-25 13:32:51 -0700

    Added d3 to users

--- middleware/error.js
+++ middleware/error.js
@@ -1,16 +1,19 @@
-const winston = require('winston')
+const winston = require("winston");
 module.exports = function(err, req, res, next) {
-    // Log the exception
-    winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}\nERROR OBJECT:\n`, err); // Metadata is in second argument
-    // winston.log('error', err.message, err);
+  // Log the exception
+  winston.error(
+    `${err.status || 500} - ${err.message} - ${req.originalUrl} - ${
+      req.method
+    } - ${req.ip}\nStack Trace:\n${err.stack}`
+  ); // Metadata is in second argument
+  // winston.log('error', err.message, err);
 
-    // error
-    // warn
-    // info
-    // verbose
-    // debug
-    // silly
+  // error
+  // warn
+  // info
+  // verbose
+  // debug
+  // silly
 
-
-    res.status(500).send('Something failed')
-}
+  res.status(500).send("Something failed");
+};
